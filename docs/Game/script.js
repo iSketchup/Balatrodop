ed0f3625ab8457c083d780cc3cadcc4bd3409161
iSketchup <kalulu@gmx.at>
2025-11-14 09:29:04 +0100

    cook

--- docs/Game/script.js
+++ docs/Game/script.js
@@ -1,13 +1,12 @@
-dragElement(document.getElementById("card"));
 
 function dragElement(elmnt) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  
+  var thisElmnt = elmnt;
     elmnt.onmousedown = dragMouseDown;
-  }
+  
+
 
   function dragMouseDown(e) {
-    e = e || window.event;
     e.preventDefault();
     // get the mouse cursor position at startup:
     pos3 = e.clientX;
@@ -18,19 +17,38 @@ function dragElement(elmnt) {
   }
 
   function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
+    e.preventDefault();    
     pos1 = pos3 - e.clientX;
     pos2 = pos4 - e.clientY;
     pos3 = e.clientX;
     pos4 = e.clientY;
-    // set the element's new position:
+
+    // set new position
     elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
     elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
   }
+  
 
   function closeDragElement() {
     // stop moving when mouse button is released:
     document.onmouseup = null;
     document.onmousemove = null;
   }
+}
+
+function build() {
+
+}
+
+function buildCard(elemnt) {
+    let card = document.createElement("div");
+    card.classList.add("card");
+    card.innerHTML = "<p>karte</p>";
+    cards.add = card;
+    elemnt.appendChild(card);
+}
+
+// dragElement(document.getElementById("GIBTS NICHT"));
+
+let cards = {};
+build();
